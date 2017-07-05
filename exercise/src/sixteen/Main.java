1cfedc6f447c94fc1cdde1468720766e8bce9a22
dandanBrave <269045975@qq.com>
2017-07-05 12:22:00 +0800

    提交华为编程第十六道题

--- exercise/src/sixteen/Main.java
+++ exercise/src/sixteen/Main.java
@@ -1,35 +1,61 @@
 package sixteen;
-
 /**
  * Created by Administrator on 2017/7/3.
- * 开发一个坐标计算工具， A表示向左移动，D表示向右移动，W表示向上移动，S表示向下移动。
- * 从（0,0）点开始移动，从输入字符串里面读取一些坐标，并将最终输入结果输出到输出文件里面。
+ 王强今天很开心，公司发给N元的年终奖。王强决定把年终奖用于购物，他把想买的物品分为两类：主件与附件，
+ 附件是从属于某个主件的，下表就是一些主件与附件的例子：
+ 主件	附件
+ 电脑	打印机，扫描仪
+ 书柜	图书
+ 书桌	台灯，文具
+ 工作椅	无
+ 如果要买归类为附件的物品，必须先买该附件所属的主件。每个主件可以有 0 个、 1 个或 2 个附件。
+ 附件不再有从属于自己的附件。王强想买的东西很多，为了不超出预算，他把每件物品规定了一个重要度，分为 5 等：
+ 用整数 1 ~ 5 表示，第 5 等最重要。他还从因特网上查到了每件物品的价格（都是 10 元的整数倍）。
+ 他希望在不超过 N 元（可以等于 N 元）的前提下，使每件物品的价格与重要度的乘积的总和最大。
+ 设第 j 件物品的价格为 v[j] ，重要度为 w[j] ，共选中了 k 件物品，编号依次为 j 1 ， j 2 ，……， j k ，则所求的总和为：
+ v[j 1 ]*w[j 1 ]+v[j 2 ]*w[j 2 ]+ … +v[j k ]*w[j k ] 。（其中 * 为乘号）
+ 请你帮助王强设计一个满足要求的购物单。
+ 输入描述:
+ 输入的第 1 行，为两个正整数，用一个空格隔开：N m
+ （其中 N （ <32000 ）表示总钱数， m （ <60 ）为希望购买物品的个数。）
+ 从第 2 行到第 m+1 行，第 j 行给出了编号为 j-1 的物品的基本数据，每行有 3 个非负整数 v p q
+ （其中 v 表示该物品的价格（ v<10000 ）， p 表示该物品的重要度（ 1 ~ 5 ）， q 表示该物品是主件还是附件。
+ 如果 q=0 ，表示该物品为主件，如果 q>0 ，表示该物品为附件， q 是所属主件的编号）
+ 输出描述:
+ 输出文件只有一个正整数，为不超过总钱数的物品的价格与重要度乘积的总和的最大值（ <200000 ）。
  */
-
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
-    while (in.hasNext()) {
-        String[] inputArr = in.next().split(";", -1);
-        int x = 0, y = 0;
-        for (int i = 0; i < inputArr.length; i++) {
-            if (inputArr[i].matches("^[ADWS]\\d{1,2}$")) {
-                String direction = inputArr[i].substring(0, 1);
-                int move = Integer.parseInt(inputArr[i].substring(1));
-                if ("A".equals(direction)) {
-                    x -= move;
-                } else if ("D".equals(direction)) {
-                    x += move;
-                } else if ("W".equals(direction)) {
-                    y += move;
-                } else {
-                    y -= move;
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            int n = sc.nextInt();      //N 表示总钱数
+            int m = sc.nextInt();       //m 表示希望购买物品的个数
+            int[] v = new int[m];       //v表示该物品的价格
+            int[] p = new int[m];       //p表示该物品的重要程度
+            int[] q = new int[m];       //q表示物品是主件还是附件
+            for (int i = 0; i <m ; i++) {
+                v[i] = sc.nextInt();
+                p[i] = sc.nextInt();
+                q[i] = sc.nextInt();
+            }
+            System.out.println(getMaxValue(v,p,q,m,n));
+        }
+    }
+
+    public static int getMaxValue(int[] val, int[] weight, int[] q, int n, int w) {
+        int[][] dp = new int[n + 1][w + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= w; j++) {
+                if (q[i-1] == 0) {  // 主件
+                    if (weight[i - 1] <= j)// 用j这么多钱去买 i 件商品 可以获得最大价值
+                        dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j- weight[i - 1]]+ val[i - 1]);
+                } else { //附件
+                        if (weight[i - 1] + weight[q[i - 1]] <= j) //附件的话 加上主件一起算
+                            dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j- weight[i - 1]]+ val[i - 1]);
                 }
             }
         }
-        System.out.println(x + "," + y);
-        }
-        in.close();
+        return dp[n][w];
     }
 }
