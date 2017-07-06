04fe9f1fd02c05f032d771fd27e21bd38b0fb919
dandanBrave <269045975@qq.com>
2017-07-06 10:31:59 +0800

    提交华为编程第一百零一道题

---
+++ exercise/src/onehundredandtwo/Main.java
@@ -0,0 +1,21 @@
+package onehundredandtwo;
+
+/**
+ * Created by Administrator on 2017/7/5.
+ * 如果统计的个数相同，则按照ASII码由小到大排序输出 。如果有其他字符，则对这些字符不用进行统计。
+ * 实现以下接口：
+ * 输入一个字符串，对字符中的各个英文字符，数字，空格进行统计（可反复调用）
+ * 按照统计个数由多到少输出统计结果，如果统计的个数相同，则按照ASII码由小到大排序输出
+ * 清空目前的统计结果，重新统计
+ * 调用者会保证：
+ * 输入的字符串以‘\0’结尾。
+ */
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()){
+            String str = sc.next();
+        }
+    }
+}
