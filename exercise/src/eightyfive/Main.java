8f1a5cdd5cab04eadea89eb4f96242e992c980bc
dandanBrave <269045975@qq.com>
2017-08-06 10:54:07 +0800

    提交华为编程第八十五道题

--- exercise/src/eightyfive/Main.java
+++ exercise/src/eightyfive/Main.java
@@ -1,9 +1,12 @@
 package eightyfive;
 
 import java.util.Scanner;
-
 /**
  * Created by Administrator on 2017/8/3.
+ * Catcher 是MCA国的情报员，他工作时发现敌国会用一些对称的密码进行通信，比如像这些ABBA，ABA，A，123321，
+ * 但是他们有时会在开始或结束时加入一些无关的字符以防止别国破解。比如进行下列变化 ABBA->12ABBA,ABA->ABAKK,
+ * 123321->51233214　。因为截获的串太长了，而且存在多种可能的情况（abaaab可看作是aba,或baaab的加密形式），
+ * Cathcer的工作量实在是太大了，他只能向电脑高手求助，你能帮Catcher找出最长的有效密码串吗？
  */
 public class Main {
 
