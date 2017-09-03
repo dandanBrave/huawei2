e30d4eefb36c130984e643f02b5fb60793c5dfec
dandanBrave <269045975@qq.com>
2017-09-03 23:33:30 +0800

    提交剑指offer从1到n的整数中1的个数

---
+++ jianzhioffer/src/ZhengShuZhong1DeGeShu.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ZhengShuZhong1DeGeShu {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(NumberOf1Between1AndN_Solution(a));
+    }
+    public static int NumberOf1Between1AndN_Solution(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += NumberOf1(i);
+        }
+        return sum;
+    }
+    public static int NumberOf1(int n){
+        int sum = 0;
+        while(n != 0){
+            if(n%10 == 1){
+                sum++;
+            }
+            n = n/10;
+        }
+        return sum;
+    }
+}
+
