9f6e08659ffc67b7291d0db41a5039c2ca4fcd0d
dandanBrave <269045975@qq.com>
2017-09-05 00:01:11 +0800

    提交公司真题构造回文

---
+++ jianzhioffer/src/ShuZuPaiChengZuiXiaoDeShu.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+/**
+ * Created by Administrator on 2017/9/4.
+ */
+public class ShuZuPaiChengZuiXiaoDeShu {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(PrintMinNumber(arr));
+    }
+    public static String PrintMinNumber(int[] numbers) {
+        ArrayList<Integer> al = new ArrayList<Integer>();
+        for (int i = 0; i < numbers.length; i++) {
+            al.add(numbers[i]);
+        }
+        Collections.sort(al, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer str1, Integer str2) {
+                String s1 = str1 + "" + str2;
+                String s2 = str2 + ""+ str1;
+                return s1.compareTo(s2);
+            }
+        });
+        String s = "";
+        for (int i:al) {
+            s += i;
+        }
+        return s;
+    }
+}
