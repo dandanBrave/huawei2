2c5a30a161f6ffd2c953b8b2d7f37e0f408b025f
dandanBrave <269045975@qq.com>
2017-09-06 23:39:13 +0800

    提交剑指offer丑数

---
+++ jianzhioffer/src/ChouShu.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/**
+ * Created by Administrator on 2017/9/6.
+ */
+public class ChouShu {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(GetUglyNumber_Solution(n));
+    }
+    public static int GetUglyNumber_Solution(int index) {
+
+        return 0;
+    }
+}
