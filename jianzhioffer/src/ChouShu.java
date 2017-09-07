d702471c832a9cfc58ace38be9095df3bd84b34a
dandanBrave <269045975@qq.com>
2017-09-07 09:07:29 +0800

    提交剑指offer

--- jianzhioffer/src/ChouShu.java
+++ jianzhioffer/src/ChouShu.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 /**
  * Created by Administrator on 2017/9/6.
+ * 复杂度高
  */
 public class ChouShu {
     public static void main(String[] args) {
@@ -10,7 +11,30 @@ public class ChouShu {
         System.out.println(GetUglyNumber_Solution(n));
     }
     public static int GetUglyNumber_Solution(int index) {
+        if(index<=0){
+            return 0;
+        }
+        int number = 0;   //判断了多少个数
+        int uglySum = 0;    //丑数的个数
+        while(uglySum<index){
+            ++number;
+            if(IsUgly(number)){
+                ++uglySum;
+            }
+        }
+        return number;
+    }
 
-        return 0;
+    public static boolean IsUgly(int number){
+        while (number%2 == 0){
+            number /= 2;
+        }
+        while (number%3 == 0){
+            number /= 3;
+        }
+        while (number%5 == 0){
+            number /= 5;
+        }
+        return number==1?true:false;
     }
 }
